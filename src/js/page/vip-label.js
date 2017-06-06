55c935d8e0268e7afd365205bf6ea0e696400f4a
webtianchen <3022435937@qq.com>
2017-06-06 19:48:27 +0800

    tc1.0

--- src/js/page/vip-label.js
+++ src/js/page/vip-label.js
@@ -1,3 +1,23 @@
 /**
  * Created by admin on 2017/6/1.
  */
+var $ = require("../lib/jquery-3.1.1.js");
+	var page = {
+		init: function() {
+			this.label_nav();
+		},
+
+			label_nav:function() {
+			$('.brands_nav_li').click(function() {
+				var bb = $(this).index();
+				//		console.log(bb);
+				//		淡入淡出
+				$(".section_main").eq(bb).fadeIn(1000).siblings(".section_main").fadeOut(1000);
+
+				$(this).addClass("here").siblings().removeClass("here"); //removeClass就是删除当前其他类；只有当前对象有addClass("selected")；siblings()意思就是当前对象的同级元素，removeClass就是删除；
+				$(".section_main").hide().eq(bb).show();
+			});
+		}
+	}
+
+	page.init();

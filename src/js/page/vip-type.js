b3bd01e9b973e168d48dc1d25570258482e6a60d
maruixiao111 <1273020337@qq.com>
2017-06-07 10:20:22 +0800

    'maruixiao2'

--- src/js/page/vip-type.js
+++ src/js/page/vip-type.js
@@ -1,3 +1,17 @@
-/**
- * Created by admin on 2017/6/1.
- */
+
+ var $=require("../lib/jquery-3.1.1.js");
+ var page={
+	init:function(){
+		this.tab();
+	},
+	tab:function(){
+		$(".classify_li").click(function(){
+			var aa = $(this).index();
+			$(this).addClass("color").siblings().removeClass();
+			$(".warp").hide().eq(aa).show();
+
+		})
+	}
+	
+}
+page.init();
